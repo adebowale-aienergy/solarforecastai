6b57f1cdc4d712f1b426a8829040f8e9b74ea988
Adebowale Immanuel Adeyemi <immanueladebowaleadeyemi@yahoo.com>
2025-08-31 19:49:43 +0100

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""SolarForecastAI src package."""
+__all__ = [
+    "constants",
+    "data_utils",
+    "geo_utils",
+    "model_utils",
+    "eval_utils",
+    "visualization",
+]
+__version__ = "0.1.0"
