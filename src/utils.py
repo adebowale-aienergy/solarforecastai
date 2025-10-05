1a9ed5196f84172ceaad551af3941469d047a8aa
Adebowale Immanuel Adeyemi <immanueladebowaleadeyemi@yahoo.com>
2025-10-05 19:15:44 +0100

    utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,10 @@
+import datetime
+
+def format_date(date_str):
+    """Format date string into datetime object."""
+    return datetime.datetime.strptime(date_str, "%Y-%m-%d")
+
+def select_country(df, country_name, col="country"):
+    """Filter dataset for a specific country."""
+    return df[df[col] == country_name]
  
